155e5659d8cf49bc3948dbeb7d5c7c21a8cc33cb
rahmadsandy <rahmad@niagahoster.co.id>
2020-10-20 14:56:31 +0700

    edit readme and fix script fix

--- collect.sh
+++ collect.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
+
 EMAIL=$1 
 for i in $(find ~/.config/google-chrome/*/Extensions -name 'manifest.json'); do n=$(grep -hIr name $i| cut -f4 -d '"'| sort);u="https://chrome.google.com/extensions/detail/";ue=$(basename $(dirname $(dirname $i))); echo -e "$n:\n$u$ue\n" ; done > ~/Desktop/$EMAIL-list.txt
